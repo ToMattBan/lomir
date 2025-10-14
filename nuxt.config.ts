e04faa0051853a266f46d686d10ba931cb6fb0ca
ToMattBan <cleyton_laz@protonmail.com>
2025-10-14 20:03:47 -0300

    db

--- nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  modules: ['nuxt-auth-utils', '@nuxt/eslint'],
-  devtools: { enabled: true },
-
-  runtimeConfig: { public: { API_URL: process.env.API_URL || 'http://localhost:8000' } },
-
-  compatibilityDate: '2025-07-15',
-});
